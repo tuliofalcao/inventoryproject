401f27295e5b84e8a4016481ac48fc6abb08cf0c
tuliofalcao <falcao.tulio@gmail.com>
2024-12-17 12:03:00 -0300

    admin user

--- inventory/dashboard/models.py
+++ inventory/dashboard/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+CATEGORIA = {
+    ('Estacionario','Estacionario'),
+    ('Eletronicos','Eletronicos'),
+    ('Alimento','Alimento'),
+}
+
+
+class Product(models.Model):
+    nome = models.CharField(max_length=100, null=True)
+    categoria = models.CharField(max_length=20, choices=CATEGORIA, null=True)
+    quantidade = models.PositiveIntegerField(null=True)
+    
+    def __str__(self):
+        return f'{self.nome} - {self.quantidade}'
