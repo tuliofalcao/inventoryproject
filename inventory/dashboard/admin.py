5f3663189b589af68d57f3f6c778f87dc5b0350a
tuliofalcao <falcao.tulio@gmail.com>
2024-12-19 19:02:34 -0300

    register

--- inventory/dashboard/admin.py
+++ inventory/dashboard/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Order
+from django.contrib.auth.models import Group
+
+
+admin.site.site_header = 'Inventário ETEMAC'
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'categoria', 'quantidade')
+    list_filter = ['categoria']
 
 
 # Register your models here.
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Order)
+#admin.site.unregister(Group)
--- inventory/dashboard/models.py
+++ inventory/dashboard/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -14,5 +15,20 @@ class Product(models.Model):
     categoria = models.CharField(max_length=20, choices=CATEGORIA, null=True)
     quantidade = models.PositiveIntegerField(null=True)
     
+    class Meta:
+        verbose_name_plural = 'Product'
+    
     def __str__(self):
         return f'{self.nome} - {self.quantidade}'
+    
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    staff = models.ForeignKey(User, models.CASCADE, null=True)
+    order_quantity = models.PositiveIntegerField(null=True)
+    date = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name_plural = 'Order'
+    
+    def __str__(self):
+        return f'{self.product} ordered by {self.staff}'
