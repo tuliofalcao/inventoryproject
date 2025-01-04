e3d05fac2df4e519d83384396153738c3475d6c8
tuliofalcao <falcao.tulio@gmail.com>
2025-01-03 23:43:02 -0300

    class Order

--- inventory/dashboard/admin.py
+++ inventory/dashboard/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Order
 from django.contrib.auth.models import Group
 
 admin.site.site_header = 'Inventário da ETEMAC'
@@ -10,4 +10,5 @@ class ProductAdmin(admin.ModelAdmin):
     
 # Register your models here.
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Order)
 # admin.site.unregister(Group)
--- inventory/dashboard/models.py
+++ inventory/dashboard/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -14,5 +15,20 @@ class Product(models.Model):
     categoria = models.CharField(max_length=20, choices=CATEGORIA, null=True)
     quantidade = models.PositiveIntegerField(null=True)
     
+    class Meta:
+        verbose_name_plural = 'Product'
+    
     def __str__(self):
         return f'{self.nome} - {self.quantidade}'
+    
+class Order(models.Model):
+    produto = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    pessoal = models.ForeignKey(User, models.CASCADE, null=True)
+    quantidade_pedido = models.PositiveIntegerField(null = True)
+    data = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name_plural = 'Order'
+    
+    def __str__(self):
+        return f'{self.produto} pedido por {self.pessoal.username}' 
