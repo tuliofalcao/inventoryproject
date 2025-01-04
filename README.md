7aed3fcc187393964946985e8ffa1624281efb49
tuliofalcao <falcao.tulio@gmail.com>
2025-01-03 23:06:30 -0300

    dashboard > admin.py > class ProductAdmin

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Professora: Rozelma França
 Aluno: Túlio Falcão
 
 Projeto de Inventário para a disciplina ESTÁGIO SUPERVISIONADO OBRIGATÓRIO II, a partir do canal do Kenbro Tech (https://www.youtube.com/@KenBroTech) em seu tutorial de Django.
+
--- inventory/dashboard/admin.py
+++ inventory/dashboard/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import Product
+from django.contrib.auth.models import Group
 
+admin.site.site_header = 'Inventário da ETEMAC'
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'categoria', 'quantidade')
+    list_filter = ['categoria']
+    
 # Register your models here.
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
+# admin.site.unregister(Group)
